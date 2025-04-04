79b62ae0091e357d7a9d7bb0d8925d67a5b8ec9b
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 02:10:46 -0500

    Seed the database with 10 products from external api

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+require 'open-uri'
+require 'json'
+
+
 AdminUser.destroy_all
 
 # Fetch admin credentials from environment variables
@@ -13,3 +17,25 @@ if admin_email.present? && admin_password.present?
 else
   puts "Admin credentials are missing. Please set ADMIN_EMAIL and ADMIN_PASSWORD environment variables."
 end
+
+
+
+
+
+# Populate database with at least 10 products from an external API
+url = 'http://makeup-api.herokuapp.com/api/v1/products.json'
+response = URI.open(url).read
+products = JSON.parse(response)
+
+products.first(10).each do |product|
+  category = Category.find_or_create_by(name: product['category'] || 'Uncategorized')
+  Product.find_or_create_by(name: product['name']) do |p|
+    p.description = product['description'] || 'No description available'
+    p.price = product['price'].to_f
+    p.stock_quantity = rand(10..100) # Random stock quantity
+    p.image_url = product['image_link']
+    p.category = category
+  end
+end
+
+puts "Products seeded successfully!"
