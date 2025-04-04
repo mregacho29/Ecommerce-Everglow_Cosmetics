e7884278b7f12b3fd0f853b2f12c59c9ac29f545
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 01:58:12 -0500

    define the ransackable_associations method in User  model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,5 @@
 class User < ApplicationRecord
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "email", "first_name", "id", "password_digest", "role", "updated_at"]
+  end
 end
