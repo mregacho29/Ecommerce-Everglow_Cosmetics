d7e3c8bf0f0b740915c0a9e969677e39293a7a57
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 01:58:57 -0500

    define the ransackable_associations method in Order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,3 +1,12 @@
 class Order < ApplicationRecord
   belongs_to :user
+
+  def self.ransackable_associations(auth_object = nil)
+    ["user"]
+  end
+
+  def self.ransackable_attributes(auth_object = nil)
+    ["created_at", "id", "status", "tax", "total_amount", "updated_at", "user_id"]
+  end
+
 end
