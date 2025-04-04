d9f4b700983d765efc11f9c70e7a82abe6be14f1
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 02:06:08 -0500

    Update app/admin/products.rb to allow managing the products

--- app/admin/products.rb
+++ app/admin/products.rb
@@ -1,18 +1,29 @@
 ActiveAdmin.register Product do
+  permit_params :name, :description, :price, :stock_quantity, :image_url, :category_id
 
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :name, :description, :price, :stock_quantity, :image_url, :category_id
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:name, :description, :price, :stock_quantity, :image_url, :category_id]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column :name
+    column :price
+    column :stock_quantity
+    column :category
+    actions
+  end
+
+  filter :name
+  filter :price
+  filter :category
+
+  form do |f|
+    f.inputs do
+      f.input :name
+      f.input :description
+      f.input :price
+      f.input :stock_quantity
+      f.input :image_url
+      f.input :category
+    end
+    f.actions
+  end
 end
