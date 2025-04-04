12e65859f1afdc287584655532b12fdf3de26f93
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 01:40:00 -0500

    Create a Product Model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.0].define(version: 2025_04_03_093714) do
+ActiveRecord::Schema[8.0].define(version: 2025_04_04_063752) do
   create_table "active_admin_comments", force: :cascade do |t|
     t.string "namespace"
     t.text "body"
@@ -43,4 +43,18 @@ ActiveRecord::Schema[8.0].define(version: 2025_04_03_093714) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
+
+  create_table "products", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.decimal "price"
+    t.integer "stock_quantity"
+    t.string "image_url"
+    t.integer "category_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["category_id"], name: "index_products_on_category_id"
+  end
+
+  add_foreign_key "products", "categories"
 end
