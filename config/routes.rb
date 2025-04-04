5872b02fcbf671db91e1df04411373f5a0396806
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-04 02:40:40 -0500

    Add resource routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,15 @@ Rails.application.routes.draw do
   ActiveAdmin.routes(self)
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  resources :products, only: [:index, :show] # Add this line for products
+  resources :categories, only: [:index, :show] # Add this line for categories
+  resources :orders, only: [:new, :create] # Add this line for orders
+  resources :cart, only: [:show] # Add this line for cart
+  resources :users, only: [:new, :create] # Add this line for users
+  resources :sessions, only: [:new, :create, :destroy] # Add this line for sessions
+  resources :addresses, only: [:new, :create] # Add this line for addresses
+
+
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
