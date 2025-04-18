865f09a90989eb51f621a77b16b1e2ed5c0556d1
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-18 03:02:41 -0500

    Add french locale languange

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,22 @@
+document.addEventListener('DOMContentLoaded', () => {
+  const searchToggle = document.getElementById('searchToggle');
+  const searchBarRow = document.getElementById('searchBarRow');
+  const searchClose = document.getElementById('searchClose');
+
+  if (searchToggle && searchBarRow && searchClose) {
+    // Show the search bar
+    searchToggle.addEventListener('click', () => {
+      searchBarRow.classList.add('show');
+      searchBarRow.classList.remove('d-none');
+    });
+
+    // Hide the search bar
+    searchClose.addEventListener('click', (e) => {
+      e.preventDefault();
+      searchBarRow.classList.remove('show');
+      setTimeout(() => {
+        searchBarRow.classList.add('d-none');
+      }, 300); // Match the CSS transition duration
+    });
+  }
+});
--- config/routes.rb
+++ config/routes.rb
@@ -1,27 +1,30 @@
 Rails.application.routes.draw do
-  get "favourites/index"
-  get "carts/show"
-  # Static pages
-  get "contact", to: "pages#show", defaults: { slug: "contact" }, as: :contact
-  get "about", to: "pages#show", defaults: { slug: "about" }, as: :about
-  get 'stores', to: 'pages#stores', as: :stores
-  get 'services', to: 'pages#services', as: :services
-  get 'cart', to: 'carts#show', as: :cart
-  get 'search', to: 'products#search', as: :search
-  get 'favourites', to: 'favourites#index', as: :favourites
+  scope "(:locale)", locale: /en|fr/ do
+    get "favourites/index"
+    get "carts/show"
 
-  # Products routes
-  resources :products, only: [:index, :show]
+    # Static pages
+    get "contact", to: "pages#show", defaults: { slug: "contact" }, as: :contact
+    get "about", to: "pages#show", defaults: { slug: "about" }, as: :about
+    get 'stores', to: 'pages#stores', as: :stores
+    get 'services', to: 'pages#services', as: :services
+    get 'cart', to: 'carts#show', as: :cart
+    get 'search', to: 'products#search', as: :search
+    get 'favourites', to: 'favourites#index', as: :favourites
 
-  devise_for :users
+    # Products routes
+    resources :products, only: [:index, :show]
 
-  # Devise and ActiveAdmin routes
-  devise_for :admin_users, ActiveAdmin::Devise.config
-  ActiveAdmin.routes(self)
+    devise_for :users
 
-  # Health check route
-  get "up" => "rails/health#show", as: :rails_health_check
+    # Devise and ActiveAdmin routes
+    devise_for :admin_users, ActiveAdmin::Devise.config
+    ActiveAdmin.routes(self)
 
-  # Root route
-  root "products#index"
+    # Health check route
+    get "up" => "rails/health#show", as: :rails_health_check
+
+    # Root route
+    root "products#index"
+  end
 end
