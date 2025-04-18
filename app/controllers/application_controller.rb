5f438ddd946d7173a173902645e1b56b127f97f5
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-18 05:14:26 -0500

    modify application_controller to resolve error for receiving invalid locale parameter

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,8 +2,20 @@ class ApplicationController < ActionController::Base
   # Only allow modern browsers supporting webp images, web push, badges, import maps, CSS nesting, and CSS :has.
   allow_browser versions: :modern
 
+  before_action :set_locale
+
   # Ensure locale is not unintentionally added to URLs
   def default_url_options
-    { locale: nil }
+    { locale: I18n.locale }
+  end
+
+  private
+
+  def set_locale
+    I18n.locale = valid_locale(params[:locale]) || I18n.default_locale
+  end
+
+  def valid_locale(locale)
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
   end
 end
