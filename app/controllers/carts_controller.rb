a0f3988fb5a4d9b3f3baf7bbb09b3155e63a583c
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-18 06:41:26 -0500

    update carts controller

--- app/controllers/carts_controller.rb
+++ app/controllers/carts_controller.rb
@@ -1,4 +1,23 @@
 class CartsController < ApplicationController
   def show
+    @cart = session[:cart] || {}
+  end
+
+  def add
+    product_id = params[:product_id]
+    quantity = params[:quantity].to_i
+
+    session[:cart] ||= {}
+    session[:cart][product_id] ||= 0
+    session[:cart][product_id] += quantity
+
+    redirect_to cart_path, notice: "Product added to cart!"
+  end
+
+  def remove
+    product_id = params[:product_id]
+    session[:cart].delete(product_id) if session[:cart]
+
+    redirect_to cart_path, notice: "Product removed from cart!"
   end
 end
