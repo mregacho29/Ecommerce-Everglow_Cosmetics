1dff45c9f5af1ac1a502b38ac5bab3cf758a1779
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-18 05:29:22 -0500

    fix admin access

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,10 +5,16 @@ class ApplicationController < ActionController::Base
   before_action :set_locale
 
   def set_locale
-    I18n.locale = params[:locale] || I18n.default_locale
+    I18n.locale = valid_locale(params[:locale]) || I18n.default_locale
   end
 
   def default_url_options
     { locale: I18n.locale }
+  end
+
+  private
+
+  def valid_locale(locale)
+    I18n.available_locales.map(&:to_s).include?(locale) ? locale : nil
   end
 end
--- config/application.rb
+++ config/application.rb
@@ -15,6 +15,8 @@ module EcommerceProject
     # not contain `.rb` files, or that should not be reloaded or eager loaded.
     # Common ones are `templates`, `generators`, or `middleware`, for example.
     config.autoload_lib(ignore: %w[assets tasks])
+    config.i18n.available_locales = [ :en, :fr ] # Add your supported locales here
+    config.i18n.default_locale = :en
 
     # Configuration for the application, engines, and railties goes here.
     #
