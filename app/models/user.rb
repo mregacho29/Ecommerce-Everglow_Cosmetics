62af3a3e694198c42351eefcaeeffae5b8c25ede
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-22 03:44:07 -0500

    Set up association between user and addresses

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,9 @@
 class User < ApplicationRecord
+  has_one :address, dependent: :destroy
+  accepts_nested_attributes_for :address
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
-
 
   def self.ransackable_attributes(auth_object = nil)
     [ "created_at", "email", "first_name", "id", "password_digest", "role", "updated_at" ]
