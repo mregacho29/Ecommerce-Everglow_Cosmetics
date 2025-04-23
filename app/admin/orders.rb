f502644dc2a40f472775d63bedcd3cf3b7d30b70
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-23 14:06:42 -0500

    Update Active Admin Orders Resource

--- app/admin/orders.rb
+++ app/admin/orders.rb
@@ -1,18 +1,27 @@
 ActiveAdmin.register Order do
-
-  # See permitted parameters documentation:
-  # https://github.com/activeadmin/activeadmin/blob/master/docs/2-resource-customization.md#setting-up-strong-parameters
-  #
-  # Uncomment all parameters which should be permitted for assignment
-  #
-  # permit_params :user_id, :total_amount, :status, :tax
-  #
-  # or
-  #
-  # permit_params do
-  #   permitted = [:user_id, :total_amount, :status, :tax]
-  #   permitted << :other if params[:action] == 'create' && current_user.admin?
-  #   permitted
-  # end
-  
+  index do
+    selectable_column
+    id_column
+    column "Customer" do |order|
+      "#{order.user.full_name} (#{order.user.email})"
+    end
+    column "Products Ordered" do |order|
+      ul do
+        order.order_items.each do |item|
+          li "#{item.product.name} - Qty: #{item.quantity} - $#{item.price}"
+        end
+      end
+    end
+    column "Tax" do |order|
+      number_to_currency(order.tax)
+    end
+    column "Total Amount" do |order|
+      number_to_currency(order.total_amount)
+    end
+    column "Grand Total" do |order|
+      number_to_currency(order.total_amount + order.tax)
+    end
+    column "Order Date", :created_at
+    actions
+  end
 end
