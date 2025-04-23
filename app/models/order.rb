6efc1017db932577e647dade1a18dd301fb6d1fb
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-23 14:05:43 -0500

    Update association on order model

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,12 +1,13 @@
 class Order < ApplicationRecord
   belongs_to :user
+  has_many :order_items
+  has_many :products, through: :order_items
 
   def self.ransackable_associations(auth_object = nil)
-    ["user"]
+    [ "user" ]
   end
 
   def self.ransackable_attributes(auth_object = nil)
-    ["created_at", "id", "status", "tax", "total_amount", "updated_at", "user_id"]
+    [ "created_at", "id", "status", "tax", "total_amount", "updated_at", "user_id" ]
   end
-
 end
