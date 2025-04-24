fdc95676f2bf382fc2c7ea4c72920b9242807f4d
Mregacho29 <mregacho@academic.rrc.ca>
2025-04-23 20:34:18 -0500

    setting up announment for admin dashboard

--- app/admin/announcement.rb
+++ app/admin/announcement.rb
@@ -1,14 +1,17 @@
 ActiveAdmin.register Announcement do
+  # Permit parameters for creating and updating announcements
   permit_params :title, :image
 
+  # Form for creating/editing announcements
   form do |f|
     f.inputs "Announcement Details" do
       f.input :title
-      f.input :image, as: :file # File upload field
+      f.input :image, as: :file # File upload field for Active Storage
     end
     f.actions
   end
 
+  # Display uploaded image in the admin show page
   show do
     attributes_table do
       row :title
